e0e67dfa025ee3a289a80ce24b8aa9a7f3c9bba9
esamson6 <77295762+esamson6@users.noreply.github.com>
2021-02-11 10:11:22 -1000

    Completed the Troubleshooting

--- XcodeTroubleshootingAssignment/SecondViewController.swift
+++ XcodeTroubleshootingAssignment/SecondViewController.swift
@@ -26,14 +26,6 @@ class SecondViewController: UIViewController {
         //change label button is prewssed the text change to "I feel confident with the Xcode troubleshooting process.
         
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
